cdd9b3a59628b12529da36bde224a17e13c9c080
Reid S <reschneyer89@gmail.com>
2021-03-09 14:50:43 -0600

    Update coordinate system

--- graphics.js
+++ graphics.js
@@ -9,16 +9,25 @@ function process(canvas, func, pObj) {
     }
 
     var ctx = canvas.getContext("2d");
+    console.log("Translating and scaling");
+    // This doesn't work to flip coords, for some reason
+    // ctx.translate(0, 1000);
+    // ctx.scale(1,-1);
     var imageData = ctx.createImageData(canvas.width,
         canvas.height);
+;
 
     for (var y = 0; y < canvas.height; y++) {
         for (var x = 0; x < canvas.width; x++) {
             var result = func(new Point(x, y));
-
-            setPixel(imageData, x, y, result);
+            var coords = cartToScreen(x,y);
+            setPixel(imageData, coords[0],coords[1], result);
         }
     }
     setPixel(imageData, pObj.coords[0], pObj.coords[1], [255,255,255,255]);
     ctx.putImageData(imageData, 0, 0);
 }
+
+function cartToScreen(px, py) {
+    return [px, -py + 1000];
+  };
